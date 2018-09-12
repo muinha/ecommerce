4575fa44cb7624880413b499b474d5884ad68738
muinha <samuel_buinha@hotmail.com>
2018-09-12 20:49:51 -0300

    ADD Projeto

---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,95 @@
+<?php 
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class User extends Model {
+
+	const SESSION = "user";
+
+	public static function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+
+			":LOGIN"=>$login
+
+		));
+
+		if(count($results) === 0)
+		{
+
+			throw new \Exception("Usúario inexistente ou senha inválidos.");
+
+		}
+
+		$data = $results[0];
+
+		if (password_verify($password, $data["despassword"]) === true)
+		{
+
+			$user = new User();
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		} else {
+
+			throw new \Exception("Usúario inexistente ou senha inválidos.");
+
+		}
+
+	}
+
+	public static function verifyLogin($inadmin = true)
+	{
+
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+		){
+			header("location: /admin/login");
+			exit;
+		}
+
+	}
+
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = null;
+
+	}
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		
+
+	}
+
+}
+
+ ?>
